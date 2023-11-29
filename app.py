1afad51f8d763b4fe59f946053ebf7ae7b80ee0c
joshuaflores <floresjoshua2603@gmail.com>
2023-11-29 12:08:32 +0000

    .

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 # MySQL Instance configurations
-app.config['MYSQL_USER'] = 'jino'
+app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'secret'
 app.config['MYSQL_DB'] = 'student'
 app.config['MYSQL_HOST'] = '34.154.116.119'
